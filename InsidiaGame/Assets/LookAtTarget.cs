85a600e50a9957fdfce5df792ef1ee05184ec5d1
ChrisClark13 <krizasan@gmail.com>
2017-10-07 20:43:17 -0600

    AI Moving in Formation
    See the "AI Test 2" Scene in the Scene/Script Testing folder to see it in action.

---
+++ InsidiaGame/Assets/LookAtTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour {
+
+    public Vector3 upDirection = Vector3.up;
+    public Transform target;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (target)
+            transform.LookAt(target, upDirection);
+	}
+}
